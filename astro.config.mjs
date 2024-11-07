7cecd82edc4b947014bdd86ed2e69ee85d6a4d39
thatONEjustin <2763180+thatONEjustin@users.noreply.github.com>
2024-11-07 10:57:01 -0600

    back to node i guess
    deno task build fails when trying to build astro without useful errors
    womp womp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import deno from "@deno/astro-adapter";
+// import deno from "@deno/astro-adapter";
+// import node from "@astrojs/node";
 import tailwind from '@astrojs/tailwind';
 import svelte from '@astrojs/svelte';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: deno(),
   integrations: [svelte(), tailwind({
     applyBaseStyles: false,
     nesting: true
-  })]
+  })],
+
+  output: 'server',
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
