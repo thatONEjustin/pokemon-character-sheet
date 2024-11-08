0be9af6d703e00b6c27f565d7c3461f4345c974d
thatONEjustin <2763180+thatONEjustin@users.noreply.github.com>
2024-11-07 18:10:37 -0600

    this is for deno deploy
    going to break the netlify stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// import deno from "@deno/astro-adapter";
-// import node from "@astrojs/node";
+import deno from "@deno/astro-adapter";
 import tailwind from '@astrojs/tailwind';
 import svelte from '@astrojs/svelte';
 
-import node from '@astrojs/node';
-
-import netlify from '@astrojs/netlify';
-
-// https://astro.build/config
 export default defineConfig({
   integrations: [svelte(), tailwind({
     applyBaseStyles: false,
     nesting: true
   })],
 
-  output: 'server',
-
-  adapter: netlify()
+  output: "server",
+  adapter: deno(),
 });
