5879adf5495c4845d6da2dc1cf90aadab715aabc
thatONEjustin <2763180+thatONEjustin@users.noreply.github.com>
2024-11-29 20:17:17 -0600

    git history

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 import deno from "@deno/astro-adapter";
-import tailwind from '@astrojs/tailwind';
 import svelte from '@astrojs/svelte';
 
 import tailwindcss from '@tailwindcss/vite';
